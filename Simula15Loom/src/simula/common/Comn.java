41fc5fabf398a20b4a2da0a2106c0800270fefcc
MyhreAndersen <o.myhre@gmail.com>
2020-03-26 13:44:19 +0100

    loom

--- Simula15Loom/src/simula/common/Comn.java
+++ Simula15Loom/src/simula/common/Comn.java
@@ -1,24 +1,22 @@
+/*
+ * (CC) This work is licensed under a Creative Commons
+ * Attribution 4.0 International License.
+ *
+ * You find a copy of the License on the following
+ * page: https://creativecommons.org/licenses/by/4.0/
+ */
 package simula.common;
-
-import simula.compiler.utilities.Util;
 
 public class Comn {
 
 	public static int getJavaVersion() {
 		String ver = System.getProperty("java.version");
-		Util.popUpError("java.version="+ver+"\n");
-		System.out.println("java.version="+ver);
 		try {
 			if (ver.startsWith("1.")) {
 				return (ver.charAt(2) - '0');
 			} else {
-//				for (int i = 0; i < ver.length(); i++) {
-//					if (ver.charAt(i) == '.') {
-						ver = ver.substring(0, 2);
-						Util.popUpError("java.version="+ver+"\n");
-						return (Integer.parseInt(ver));
-//					}
-//				}
+				ver = ver.substring(0, 2);
+				return (Integer.parseInt(ver));
 			}
 		} catch (Exception e) {}
 		return (0);
